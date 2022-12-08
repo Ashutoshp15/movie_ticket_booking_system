9c41d9f91145a755fad24d80273a2925f267973c
Ashutosh Patil <ashutoshjpatil1998@gmail.com>
2022-12-07 23:51:39 -0800

    changed connection credentials in db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 
-$host = "localhost";
-$dbuser = "root";
-$dbpass = "";
+$host = "database-1.cnmy5rtnio8w.us-east-1.rds.amazonaws.com";
+$dbuser = "admin";
+$dbpass = "password";
 $db = "movie_booking";
 
 $conn = mysqli_connect($host, $dbuser, $dbpass, $db);
